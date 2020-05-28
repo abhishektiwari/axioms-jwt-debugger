9e9c9c8da71a56e0b080fedf5232c306bc02fafa
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2020-05-29 00:37:40 +1000

    Added readme and download banner for win and linux

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Axioms JWT Debugger
 Parse and validate a JSON Web Token (JWT).
 
-- Web version with progress web app support
-- Desktop version for Windows, Mac, and Linux (Debian)
+- Web version with offline support using progress web app 
+
+![Web version](axioms-jwt-debugger-web.jpg)
+
+- Desktop versions for Windows, Mac, and Linux (Debian)
+
+![Web version](axioms-jwt-debugger-desktop.jpg)
 
 # Token Validation
 
-- Using JSON Web Key Set (JWKS) Endpoint for token signed using RSA and ECDSA algorithms.
-- Using Secret Key for token signed using HMAC algorithms.
+- Using JSON Web Key Set (JWKS) Endpoint for token signed using `RSA` and `ECDSA` algorithms.
+- Using Secret Key for token signed using `HMAC` algorithms.
+
+# Bug and feature request
+Please use Github issues to create any bugs or feature request.
+
--- src/components/DownloadBanners.vue
+++ src/components/DownloadBanners.vue
@@ -10,7 +10,39 @@
           icon="fas fa-download"
           @click="
             launch(
-              'https://github.com/axioms-io/axioms-jwt-debugger/releases/download/0.0.1/JWTDebugger.app.dmg'
+              'https://github.com/axioms-io/axioms-jwt-debugger/releases/download/0.0.2/axioms-jwt-debugger.app_0.0.2.dmg'
+            )
+          "
+        />
+      </q-banner>
+    </div>
+    <div class="col" v-if="$q.platform.is.desktop && $q.platform.is.win">
+      <q-banner rounded class="bg-grey-3">
+        Download the JWT Debugger for Windows Desktop
+        <q-btn
+          unelevated
+          round
+          color="primary"
+          icon="fas fa-download"
+          @click="
+            launch(
+              'https://github.com/axioms-io/axioms-jwt-debugger/releases/download/0.0.2/axioms_jwt_debugger-0.0.2-setup.exe'
+            )
+          "
+        />
+      </q-banner>
+    </div>
+    <div class="col" v-if="$q.platform.is.desktop && $q.platform.is.linux">
+      <q-banner rounded class="bg-grey-3">
+        Download the JWT Debugger for Debian Desktop
+        <q-btn
+          unelevated
+          round
+          color="primary"
+          icon="fas fa-download"
+          @click="
+            launch(
+              'https://github.com/axioms-io/axioms-jwt-debugger/releases/download/0.0.2/axioms-jwt-debugger_0.0.2_all.deb'
             )
           "
         />
